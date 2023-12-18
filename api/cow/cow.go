390d2d1fc98b19f5cc9ed9d60faf0cf218f2d9b2
Jemil Ezzet <jemil.ezzet@gmail.com>
2023-12-18 07:50:07 -0800

    feat(init): add api

---
+++ api/cow/cow.go
@@ -0,0 +1,248 @@
+package cow
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/withtally/tally/base"
+	"github.com/withtally/tally/base/types"
+)
+
+type IClient interface {
+	Quote(ctx context.Context, chainID types.ChainID, req *QuoteRequest) (*QuoteResponse, error)
+	NativePrice(context.Context, types.EVMAccountID) (*NativePriceResponse, error)
+	Orders(context.Context, types.ChainID, common.Address) ([]Order, error)
+}
+
+type Client struct{}
+
+var (
+	// https://api.cow.fi/mainnet/api/v1/token/:address/native_price
+	nativePriceURL = "https://api.cow.fi/%s/api/v1/token/%s/native_price"
+
+	// https://api.cow.fi/mainnet/api/v1/quote
+	quoteURL = "https://api.cow.fi/%s/api/v1/quote"
+
+	// https://api.cow.fi/mainnet/api/v1/account/:address/orders
+	ordersURL = "https://api.cow.fi/%s/api/v1/account/%s/orders"
+
+	ErrSellAmountDoesNotCoverFee = "SellAmountDoesNotCoverFee"
+
+	MilkmanABI = `[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"orderContract","type":"address"},{"indexed":false,"internalType":"address","name":"orderCreator","type":"address"},{"indexed":false,"internalType":"uint256","name":"amountIn","type":"uint256"},{"indexed":false,"internalType":"address","name":"fromToken","type":"address"},{"indexed":false,"internalType":"address","name":"toToken","type":"address"},{"indexed":false,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"address","name":"priceChecker","type":"address"},{"indexed":false,"internalType":"bytes","name":"priceCheckerData","type":"bytes"}],"name":"SwapRequested","type":"event"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"contract IERC20","name":"fromToken","type":"address"},{"internalType":"contract IERC20","name":"toToken","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"address","name":"priceChecker","type":"address"},{"internalType":"bytes","name":"priceCheckerData","type":"bytes"}],"name":"cancelSwap","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract IERC20","name":"fromToken","type":"address"},{"internalType":"bytes32","name":"_swapHash","type":"bytes32"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"orderDigest","type":"bytes32"},{"internalType":"bytes","name":"encodedOrder","type":"bytes"}],"name":"isValidSignature","outputs":[{"internalType":"bytes4","name":"","type":"bytes4"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"contract IERC20","name":"fromToken","type":"address"},{"internalType":"contract IERC20","name":"toToken","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"address","name":"priceChecker","type":"address"},{"internalType":"bytes","name":"priceCheckerData","type":"bytes"}],"name":"requestSwapExactTokensForTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"swapHash","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"}]`
+)
+
+func NewClient() *Client {
+	return &Client{}
+}
+
+type Quote struct {
+	SellToken         string `json:"sellToken"`
+	BuyToken          string `json:"buyToken"`
+	SellAmount        string `json:"sellAmount"`
+	BuyAmount         string `json:"buyAmount"`
+	ValidTo           uint64 `json:"validTo"`
+	AppData           string `json:"appData"`
+	FeeAmount         string `json:"feeAmount"`
+	Kind              string `json:"kind"`
+	PartiallyFillable bool   `json:"partiallyFillable"`
+	SellTokenBalance  string `json:"sellTokenBalance"`
+	BuyTokenBalance   string `json:"buyTokenBalance"`
+	SigningScheme     string `json:"signingScheme"`
+}
+
+type QuoteResponse struct {
+	Quote       Quote     `json:"quote"`
+	From        string    `json:"from"`
+	Expiration  time.Time `json:"expiration"`
+	ID          uint64    `json:"id"`
+	ErrorType   string    `json:"errorType"`
+	Description string    `json:"description"`
+}
+
+type QuoteRequest struct {
+	SellToken           string `json:"sellToken"`
+	BuyToken            string `json:"buyToken"`
+	PartiallyFillable   bool   `json:"partiallyFillable"`
+	From                string `json:"from"`
+	Kind                string `json:"kind"`
+	SellAmountBeforeFee string `json:"sellAmountBeforeFee"`
+}
+
+func (c *Client) Quote(ctx context.Context, chainID types.ChainID, req *QuoteRequest) (*QuoteResponse, error) {
+	_, log := base.LogFor(ctx)
+
+	chainName, err := chainIDToName(chainID)
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("getting chain name")
+	}
+
+	marshaled, err := json.Marshal(req)
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("marhsaling quote request")
+	}
+
+	body := bytes.NewReader(marshaled)
+	res, err := http.Post(fmt.Sprintf(quoteURL, chainName), "application/json", body)
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("making quote request")
+	}
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("reading quote response body")
+	}
+
+	quote := new(QuoteResponse)
+	if err := json.Unmarshal(resBody, quote); err != nil {
+		return nil, fmt.Errorf("unmarshaling quote response %s", string(resBody))
+	}
+
+	if quote.ErrorType != "" {
+		return nil, errors.New(quote.ErrorType)
+	}
+
+	return quote, nil
+}
+
+type NativePriceResponse struct {
+	ErrorType   *string  `json:"errorType"`
+	Description *string  `json:"description"`
+	Price       *float64 `json:"price"`
+}
+
+func (c *Client) NativePrice(ctx context.Context, tokenID types.EVMAccountID) (*NativePriceResponse, error) {
+	_, log := base.LogFor(ctx)
+
+	chainName, err := chainIDToName(tokenID.ChainID)
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("getting chain name")
+	}
+
+	res, err := http.Get(fmt.Sprintf(nativePriceURL, chainName, tokenID.Address().Hex()))
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("getting native price data")
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("reading native price data")
+	}
+
+	price := &NativePriceResponse{}
+	if err := json.Unmarshal(body, &price); err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("marshalling native price data")
+	}
+
+	if price.ErrorType != nil {
+		return nil, errors.New(*price.Description)
+	}
+
+	return price, nil
+}
+
+type Order struct {
+	UID       string `json:"uid"`
+	Status    string `json:"status"`
+	BuyAmount string `json:"buyAmount"`
+}
+
+func (c *Client) Orders(ctx context.Context, chainID types.ChainID, address common.Address) ([]Order, error) {
+	_, log := base.LogFor(ctx)
+
+	chainName, err := chainIDToName(chainID)
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("getting chain name")
+	}
+
+	res, err := http.Get(fmt.Sprintf(ordersURL, chainName, address.Hex()))
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("getting orders data")
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("reading orders data")
+	}
+
+	var orders []Order
+	if err := json.Unmarshal(body, &orders); err != nil {
+		log.Err(err)
+		return nil, fmt.Errorf("marshalling native price data")
+	}
+
+	return orders, nil
+}
+
+func chainIDToName(chainID types.ChainID) (string, error) {
+	if chainID.Reference == "1" {
+		return "mainnet", nil
+	}
+
+	return "", errors.New("invalid chain")
+}
+
+type FakeClient struct {
+	quoteError bool
+}
+
+func NewFakeClient() *FakeClient {
+	return new(FakeClient)
+}
+
+func (fcc *FakeClient) SetQuoteError() {
+	fcc.quoteError = true
+}
+
+func (fcc *FakeClient) Quote(ctx context.Context, chainID types.ChainID, req *QuoteRequest) (*QuoteResponse, error) {
+	if fcc.quoteError {
+		return nil, errors.New(ErrSellAmountDoesNotCoverFee)
+	}
+
+	return quoteResponse, nil
+}
+
+func (fcc *FakeClient) NativePrice(ctx context.Context, tokenID types.EVMAccountID) (*NativePriceResponse, error) {
+	return &NativePriceResponse{}, nil
+}
+
+func (fcc *FakeClient) Orders(ctx context.Context, chainID types.ChainID, address common.Address) ([]Order, error) {
+	return []Order{}, nil
+}
+
+type GetSwapTokensResponse struct {
+	Tokens []SwapToken `json:"tokens"`
+}
+
+func (fcc *FakeClient) GetSwapTokens() (interface{}, error) {
+	res, err := http.Get("https://raw.githubusercontent.com/cowprotocol/token-lists/develop/src/public/CowSwap.json")
+	if err != nil {
+		return nil, fmt.Errorf("getting native price data")
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading native price data")
+	}
+
+	r := &GetSwapTokensResponse{}
+	if err := json.Unmarshal(body, &r); err != nil {
+		return nil, fmt.Errorf("marshalling native price data")
+	}
+
+	return nil, nil
+}
---
+++ api/graph/tally-proposal.resolvers.go
@@ -0,0 +1,143 @@
+package graph
+
+// AvailableSwaps is the resolver for the availableSwaps field.
+func (r *queryResolver) AvailableSwaps(ctx context.Context, governorID *proto.AccountID) (*model.SwapAssets, error) {
+	gid, err := governorID.ToCAIP()
+	if err != nil {
+		log.Err(err).Msg("Converting governor id to types.")
+		return nil, status.Error(codes.InvalidArgument, "invalid governor id")
+	}
+
+	g, err := r.EtlEnt.Governance.Get(ctx, gid)
+	if err != nil {
+		if etlent.IsNotFound(err) {
+			return nil, status.Error(codes.NotFound, "governor not found")
+		}
+		log.Err(err).Msg("getting governor")
+		return nil, status.Error(codes.Internal, ErrInternalServerError)
+	}
+
+	bs, err := r.Covalent.Balances(ctx, g.TreasuryID())
+	if err != nil {
+		log.Err(err).Msg("Getting balances.")
+		return nil, status.Error(codes.Internal, ErrInternalServerError)
+	}
+
+	assets := new(model.SwapAssets)
+
+	for _, b := range bs.Data.Items {
+		b := b
+		tid, err := types.NewEVMAccountID(gid.ChainID, b.ContractAddress)
+		if err != nil {
+			log.Err(err).Msg("Creating account id from balance item.")
+			return nil, status.Error(codes.Internal, ErrInternalServerError)
+		}
+
+		// FOR BETA release, limit sellable assets to those listed in cow.SwapTokensMap
+
+		// token is sellable asset if CowSwap api returns a native price
+		// if _, err := r.Cow.NativePrice(ctx, tid); err == nil {
+		// 	assets.Sell = append(assets.Sell, &b)
+		// }
+
+		// token is sellable asset if is listed in cow.SwapTokensMap or is Ether
+		protoTID := proto.AccountIDToProto(tid.AccountID)
+		if _, ok := cow.SwapTokensMap[protoTID.String()]; ok || b.ContractName == "Ether" {
+			assets.Sell = append(assets.Sell, &b)
+		}
+	}
+
+	for _, t := range cow.SwapTokens {
+		t := t
+		if gid.ChainID.Reference == t.ID.GetChainId().Reference {
+			assets.Buy = append(assets.Buy, &t)
+		}
+	}
+
+	return assets, nil
+}
+
+// QuoteSwap is the resolver for the quoteSwap field.
+func (r *queryResolver) QuoteSwap(ctx context.Context, governorID *proto.AccountID, buy *proto.AccountID, sell *proto.AccountID, sellAmount types.Uint256) (*model.SwapQuote, error) {
+	gid, err := governorID.ToCAIP()
+	if err != nil {
+		log.Err(err).Msg("Converting governor id to types.")
+		return nil, status.Error(codes.InvalidArgument, "invalid governor id")
+	}
+
+	g, err := r.EtlEnt.Governance.Get(ctx, gid)
+	if err != nil {
+		if etlent.IsNotFound(err) {
+			return nil, status.Error(codes.NotFound, "governor not found")
+		}
+		log.Err(err).Msg("getting governor")
+		return nil, status.Error(codes.Internal, ErrInternalServerError)
+	}
+
+	if _, ok := cow.SwapTokensMap[buy.String()]; !ok {
+		log.Err(fmt.Errorf("could not find token")).Msgf("Address: %s", buy.String())
+		return nil, status.Error(codes.InvalidArgument, "buy token not availble")
+	}
+
+	q, err := getSwapQuote(ctx, r.Cow, r.Covalent, gid.ChainID, sell.Address, buy.Address, g.TreasuryID().Address().Hex(), sellAmount.String())
+	if err != nil {
+		if err.Error() == cow.ErrSellAmountDoesNotCoverFee {
+			return nil, status.Error(codes.InvalidArgument, "sell amount is lower than the fee")
+		}
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return q, nil
+}
+
+func getSwapQuote(ctx context.Context, cowClient cow.IClient, covClient covalent.IClient, chainID types.ChainID, sellToken, buyToken, from, sellAmount string) (*model.SwapQuote, error) {
+	q, err := cowClient.Quote(ctx, chainID, &cow.QuoteRequest{
+		SellToken:           sellToken,
+		BuyToken:            buyToken,
+		From:                from,
+		Kind:                "sell",
+		SellAmountBeforeFee: sellAmount,
+	})
+	if err != nil {
+		if err.Error() == cow.ErrSellAmountDoesNotCoverFee {
+			return nil, err
+		}
+
+		log.Err(err).Msg("Getting cowswap quote.")
+		return nil, fmt.Errorf("getting swap quote")
+	}
+
+	tp, err := covClient.TokenPrice(ctx, chainID.Reference, buyToken, time.Now())
+	if err != nil {
+		log.Err(err).Msg("Getting token price.")
+		return nil, fmt.Errorf("getting buy token price")
+	}
+
+	buyTokenQuoteRate := new(float64)
+	if tp != nil {
+		buyTokenQuoteRate = tp.LastPrice()
+	}
+
+	buyAmount, err := types.ParseUint256(q.Quote.BuyAmount)
+	if err != nil {
+		return nil, err
+	}
+
+	selAmount, err := types.ParseUint256(q.Quote.SellAmount)
+	if err != nil {
+		return nil, err
+	}
+
+	feeAmount, err := types.ParseUint256(q.Quote.FeeAmount)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.SwapQuote{
+		BuyAmount:         *buyAmount,
+		BuyTokenQuoteRate: buyTokenQuoteRate,
+		SellAmount:        *selAmount,
+		FeeAmount:         *feeAmount,
+		ValidTo:           timestamppb.New(time.Unix(int64(q.Quote.ValidTo), 0)),
+	}, nil
+}
